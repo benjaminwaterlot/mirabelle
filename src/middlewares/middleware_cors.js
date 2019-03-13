74925af076cf7a1f067b43bfb9d0ac6ad037e826
Benjamin WATERLOT <waterlotbenjamin@gmail.com>
2019-03-13 20:19:07 +0100

    allow cors for all origins for now

--- src/middlewares/middleware_cors.js
+++ src/middlewares/middleware_cors.js
@@ -1,5 +1,8 @@
 const cors = require('cors');
 
-export default cors({
-	origin: 'http://localhost:8080',
-});
+export default cors();
+
+// TODO : ENABLE MULTIPLE CORS ORIGINS (DEV AND PROD)
+// export default cors({
+// 	origin: 'http://localhost:8080',
+// });
